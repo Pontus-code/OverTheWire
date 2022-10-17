b201b09b1f5033c841fb0ed7b7dd3cd0c5bb6287
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-10-17 08:22:13 +0200

    Update and rename bandit_level_1.txt to bandit_level_1.sh

--- bandit_level_1.sh
+++ bandit_level_1.sh
@@ -1,9 +1,11 @@
 # The solution to Bandit Level 0 ond 1 on OverTheWire.org
 
 # Connect with ssh to bandit.labs.overthewire.org on port 2220.
-ssh bandit0@bandit.labs.overthewire.org -p 2220
-
+# ssh bandit0@bandit.labs.overthewire.org -p 2220
 # The password is bandit0.
+
+# Run script when logged on as bandit0 
+# ./bandit_level_1.sh
 
 # List the files in the home directory.
 ls
