13a2133bef839183e7163a8c56cdcd8a9dcea7e9
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-11 21:55:15 +0100

    Add files via upload

--- bandit_level_18.sh
+++ bandit_level_18.sh
@@ -7,4 +7,6 @@
 # Goals:
 # There are 2 files in the homedirectory: passwords.old and passwords.new. The password for the next level is in passwords.new and is the only line that has been changed between passwords.old and passwords.new.
 
+diff passwords.old passwords.new
 
+# The password for bandit18 is: hga5tuuCLF6fFzUpnagiMN8ssu9LFrdg 
