30e2653ee50c6472492128b81d1574f8251a4054
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-11 22:20:00 +0100

    Add files via upload

---
+++ bandit_level_19.sh
@@ -0,0 +1,11 @@
+# Solution to Bandit level 19 on OverTheWire.org.
+
+# Goals:
+# The password for the next level is stored in a file readme in the homedirectory. Unfortunately, someone has modified .bashrc to log you out when you log in with SSH.
+
+# Connect as bandit18 and read the file before your automatically logged out.
+# ssh bandit18@bandit.labs.overthewire.org -p 2220 'cat readme'
+# The password is: hga5tuuCLF6fFzUpnagiMN8ssu9LFrdg
+
+
+# The password for bandit19 is: awhqfNnAbc1naukrpqDYcF95h7HoMTrC
---
+++ bandit_level_20.sh
@@ -0,0 +1,12 @@
+# Solution to Bandit level 20 on OverTheWire.org.
+
+# Goals:
+# To gain access to the next level, you should use the setuid binary in the homedirectory. Execute it without arguments to find out how to use it. The password for this level can be found in the usual place (/etc/bandit_pass), after you have used the setuid binary.
+
+# Connect as bandit19
+# ssh bandit19@bandit.labs.overthewire.org -p 2220
+# The password is: awhqfNnAbc1naukrpqDYcF95h7HoMTrC
+
+./bandit20-do cat /etc/bandit_pass/bandit20
+
+# The pasword for bandit20 is: VxCazJaVykI6W36BkBU0mJTCM8rR95XT
