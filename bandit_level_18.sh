9aeae91658dc94e936af12d907bcbeb9279f6276
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-11 21:54:41 +0100

    Add files via upload

---
+++ bandit_level_18.sh
@@ -0,0 +1,10 @@
+# Solution to Bandit level 18 on OverTheWire.org.
+
+# Connect as bandit17.
+# ssh bandit17@bandit.labs.overthewire.org -p 2220
+# The password is: VwOSWtCA7lRKkTfbr2IDh6awj9RNZM5e
+
+# Goals:
+# There are 2 files in the homedirectory: passwords.old and passwords.new. The password for the next level is in passwords.new and is the only line that has been changed between passwords.old and passwords.new.
+
+
